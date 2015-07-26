b0abd08d131d78ba4cff303469f3de308c17cd72
Eridanus_Sora <h@moe.re>
2015-07-26 19:55:48 +0800

    数据分析

---
+++ statistics/data.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* Wikiplus 数据分析系统
+*/
+
+//数据库信息定义
+/*
+$DB_Host     = 'localhost';
+$DB_Name     = 'wikiplus';
+$DB_UserName = 'root';
+$DB_UserPass = '';
+*/
+
+$DB_Host     = 'localhost';
+$DB_Name     = MYSQL_DATABASE;
+$DB_UserName = MYSQL_USERNAME;
+$DB_UserPass = MYSQL_PASSWORD;
+?>
