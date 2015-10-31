7dcbf03488f78983ceeab4d984b55b4f2c00d071
Eridanus_Sora <haolinaihui@gmail.com>
2015-11-01 07:50:12 +0800

    fix

--- languages/get.php
+++ languages/get.php
@@ -1,12 +1,45 @@
 <?php
+//gzip prepare
+if(preg_match('/gzip/',$_SERVER['HTTP_ACCEPT_ENCODING'])){
+	ob_start('ob_gzhandler');
+}else{
+	ob_start();
+}
+//add headers
 header('Access-Control-Allow-Origin: *');
-if (isset($_GET['lang'])){
-	$language = $_GET['lang'];
-	if (preg_match("/^[0-9a-z\-]+$/i",$language)){
-		echo file_get_contents('./' . $language . '.json');
-	}
-	else{
-		die('?');
+header('Content-Type: text/javascript');
+
+echo checkAndreturn();
+ob_flush();
+
+	
+function checkAndreturn(){
+	if (isset($_GET['lang'])){
+		$language = $_GET['lang'];
+		if (preg_match("/^[0-9a-z\-]+$/i",$language)){
+			return findLangFile($language);
+		}
+		else{
+			return "?";
+		}
+	}else{
+		return "?";
 	}
 }
+
+function findLangFile($language){
+	$langJson = file_get_contents('./' . $language . '.json');
+	if($langJson === FALSE){
+		return "?";
+	}
+	$langHash = md5($langJson);
+	if(isset($_SERVER['HTTP_IF_NONE_MATCH']) && $langHash == $_SERVER['HTTP_IF_NONE_MATCH']){
+		header('Etag: '.$langHash, true, 304);
+		return "";
+	}else{
+		header('Etag: '.$langHash);
+		return $langJson;
+	}
+}
+
 ?>
