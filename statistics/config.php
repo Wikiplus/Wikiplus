011f58dbc9b6bd882ea335cac421501e0b9c5a4a
Eridanus_Sora <haolinaihui@gmail.com>
2015-11-13 21:03:48 +0800

    update config

--- statistics/config.php
+++ statistics/config.php
@@ -1,6 +1,12 @@
 <?php 
-$DB_Host     = 'localhost';
-$DB_Name     = MYSQL_DATABASE || 'wikiplus';
-$DB_UserName = MYSQL_USERNAME || 'root';
-$DB_UserPass = MYSQL_PASSWORD || '';
+
+	$DB_Host     = 'localhost';
+	$DB_Name     = MYSQL_DATABASE;
+	$DB_UserName = MYSQL_USERNAME;
+	$DB_UserPass = MYSQL_PASSWORD;
+
+	// $DB_Name     = 'wikiplus';
+	// $DB_UserName = 'root';
+	// $DB_UserPass = '';
+
 ?>
