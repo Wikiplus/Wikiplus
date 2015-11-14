89b2423b0b02b9ed2adda47c58d1917ba66d0e2e
zyzsdy <zyzsdy@foxmail.com>
2015-11-14 10:25:55 +0800

    修改gruntfile.js和package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,4 +35,5 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['babel','uglify']);
 }
