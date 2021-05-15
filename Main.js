cb6f2acac6164425f8c6b0072627c11e82689f98
安忆 <i@anyi.in>
2021-05-15 17:14:06 +0800

    Update Main.js
    为Minerva皮肤加入了一些样式

--- Main.js
+++ Main.js
@@ -1041,9 +1041,7 @@ $(function () {
                                     target: sectionTargetName
                                 });
                                 if (mw.config.get('skin') === 'minerva') {
-                                    mw.loader.using('mediawiki.util').done(function() {
-                                        mw.util.addCSS('.mw-parser-output .mw-editsection{display:flex!important;align-items:center}.mw-parser-output .section-heading>div{vertical-align:baseline}');
-                                    });
+                                    mw.loader.addStyleTag('.mw-parser-output .mw-editsection{display:flex!important;align-items:center}.mw-parser-output .section-heading>div{vertical-align:baseline}');
                                     $(this).append(_sectionBtn);
                                 } else {
                                     $(this).find('.mw-editsection-bracket').last().before(_sectionBtn);
