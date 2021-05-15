e0bd26ebf3b7107afceafb23115aff24cbf6908f
安忆 <i@anyi.in>
2021-05-15 13:56:44 +0800

    Update Main.js
    为Minerva皮肤加入了一些样式
    不对MetaWiki桌面视图插入段落编辑按钮
    修改了i18n的一些拼写

--- Main.js
+++ Main.js
@@ -95,7 +95,7 @@ $(function () {
     i18nData['zh-cn'] = {
         "__language": "zh-cn",
         "__author": ["Eridanus Sora"],
-        "__version": "211",
+        "__version": "212",
         "unknown_error_name": "未知的错误名",
         "api_unaccessiable": "无可用的API",
         "api_unwriteable": "无可用的写入API",
@@ -156,7 +156,7 @@ $(function () {
         "customjsprotected": "无法编辑用户JS页",
         "cascadeprotected": "该页面被级联保护",
         "network_edit_error": "由于网络原因编辑失败",
-        "redirect_to_summary": "重定向页面至 [[$1]] \/\/ Wikiplus",
+        "redirect_to_summary": "重定向页面至[[$1]] \/\/ Wikiplus",
         "redirect_from_summary": "将[[$1]]重定向至[[$2]] \/\/ Wikiplus",
         "need_init": "页面类未加载完成",
         "fail_to_get_wikitext": "无法获得页面文本",
@@ -172,7 +172,7 @@ $(function () {
         "preview": "预览",
         "cancel": "取消",
         "mark_minoredit": "标记为小编辑",
-        "onclose_confirm": "[Wikiplus] 您确认要关闭/刷新页面吗 这会导致您的编辑数据丢失",
+        "onclose_confirm": "[Wikiplus] 您确认要关闭/刷新页面吗？这会导致您的编辑数据丢失",
         "fail_to_get_wikitext_when_edit": "无法获得页面文本以编辑",
         "cant_parse_wikitext": "无法解析维基文本",
         "loading_preview": "正在读取预览",
@@ -182,7 +182,7 @@ $(function () {
         "cross_page_edit": "编辑目标位于其他页面 正在获取基础信息",
         "cross_page_edit_submit": "基础信息获取成功 正在提交编辑",
         "cross_page_edit_error": "无法获得基础信息>.<",
-        "install_tip": "您是否允许Wikiplus采集非敏感数据用于改进Wikiplus及为当前Wiki: $1 提供改进建议?",
+        "install_tip": "您是否允许Wikiplus采集非敏感数据用于改进Wikiplus及为当前Wiki：$1提供改进建议？",
         "accept": "接受",
         "decline": "拒绝",
         "install_finish": "Wikiplus安装完毕",
@@ -209,7 +209,7 @@ $(function () {
     i18nData['en-us'] = {
         "__language": "en-us",
         "__author": ["Eridanus Sora", "AnnAngela", "YinYan"],
-        "__version": "211",
+        "__version": "212",
         "unknown_error_name": "Unknown error",
         "api_unaccessiable": "API of this wiki is not available",
         "api_unwriteable": "Write API of this wiki is not available",
@@ -272,11 +272,11 @@ $(function () {
         "network_edit_error": "Failed to edit this page because of network errors",
         "redirect_to_summary": "Redirect to [[$1]] \/\/ Wikiplus",
         "redirect_from_summary": "Redirect [[$1]] to [[$2]] \/\/ Wikiplus",
-        "need_init": "WikiPlus haven't been loaded completely. It's a rare occasion so you can try to refresh and try again.",
+        "need_init": "Wikiplus haven't been loaded completely. It's a rare occasion so you can try to refresh and try again.",
         "fail_to_get_wikitext": "Failed to load the wikitext of this page",
         "quickedit_topbtn": "QuickEdit",
         "quickedit_sectionbtn": "QuickEdit",
-        "fail_to_init_quickedit": "Failed to initialize WikiPlus",
+        "fail_to_init_quickedit": "Failed to initialize Wikiplus",
         "back": "Back",
         "goto_editbox": "Jump to editbox",
         "summary_placehold": "Edit summary",
@@ -296,12 +296,12 @@ $(function () {
         "cross_page_edit": "The content you are editing belongs to another page, please wait...",
         "cross_page_edit_submit": "Submitting your edit...",
         "cross_page_edit_error": "Failed to load the infomation",
-        "install_tip": "Do you allow WikiPlus to collect insensitive data to help us develop WikiPlus and provide feedback to current site: $1 ?",
+        "install_tip": "Do you allow Wikiplus to collect insensitive data to help us develop Wikiplus and provide feedback to current site: $1 ?",
         "accept": "Yes",
         "decline": "No",
         "install_finish": "Wikiplus is installed, enjoy it",
         "loading": "Loading",
-        "cant_add_funcbtn": "Failed to add buttons for WikiPlus",
+        "cant_add_funcbtn": "Failed to add buttons for Wikiplus",
         "wikiplus_settings": "Wikiplus Setting",
         "wikiplus_settings_desc": "Please modify your setting according to the standards below",
         "wikiplus_settings_placeholder": "Your setting is empty, please modify your setting according to the documentation.",
@@ -649,7 +649,7 @@ $(function () {
             console.log('页面类构建中');
             //可用性和权限检测
             if (!window.mw) {
-                console.log('页面Javascript载入不完全或这不是一个Mediawiki站点');
+                console.log('页面JavaScript载入不完全或这不是一个MediaWiki站点');
                 return;
             }
             if (!inArray('autoconfirmed', window.mw.config.get('wgUserGroups'))) {
@@ -1007,13 +1007,21 @@ $(function () {
                         number: -1,
                         target: self.kotori.pageName
                     });
-                    if ($('#ca-edit').length > 0 && $('#Wikiplus-Edit-TopBtn').length == 0) {
-                        $('#ca-edit').before(topBtn);
+                    if (mw.config.get('skin') === 'minerva') {
+                        $(topBtn).css({ 'align-items': 'center', 'display': 'flex' });
+                        $(topBtn).find('span').addClass('page-actions-menu__list-item');
+                        $(topBtn).find('a').css('vertical-align', 'middle');
+                        $(topBtn).find('a').addClass('mw-ui-icon mw-ui-icon-element mw-ui-icon-wikimedia-edit-base20 mw-ui-icon-with-label-desktop');
                     }
-                    if ($('.mw-editsection').length > 0) {
+                    if ($('#ca-edit').length > 0 && $('#Wikiplus-Edit-TopBtn').length == 0) {
+                        mw.config.get('skin') === 'minerva' ? $('#ca-edit').parent().after(topBtn) : $('#ca-edit').after(topBtn);
+                    } else {
+                        throwError('fail_to_init_quickedit');
+                    }
+                    if ($('.mw-editsection').length > 0 && !/meta\.wikimedia\.org/.test(location.host) && mw.config.get('skin') !== 'minerva') {
                         self.sectionMap = {};
                         //段落快速编辑按钮
-                        var sectionBtn = $('<span>').append($('<span>').addClass('mw-editsection-divider').text(' | ')).append($('<a>').addClass('Wikiplus-Edit-SectionBtn').attr('href', 'javascript:void(0)').text(i18n('quickedit_sectionbtn')));
+                        var sectionBtn = mw.config.get('skin') === 'minerva' ? $('<span>').append($('<a>').addClass('Wikiplus-Edit-SectionBtn mw-ui-icon mw-ui-icon-element mw-ui-icon-wikimedia-edit-base20 edit-page mw-ui-icon-flush-right').css('margin-left', '0.75em').attr('href', 'javascript:void(0)').attr('title', i18n('quickedit_sectionbtn'))) : $('<span>').append($('<span>').addClass('mw-editsection-divider').text(' | ')).append($('<a>').addClass('Wikiplus-Edit-SectionBtn').attr('href', 'javascript:void(0)').text(i18n('quickedit_sectionbtn')));
                         $('.mw-editsection').each(function (i) {
                             try {
                                 var editURL = $(this).find("a").last().attr('href');
@@ -1032,7 +1040,12 @@ $(function () {
                                     name: sectionName,
                                     target: sectionTargetName
                                 });
-                                $(this).find('.mw-editsection-bracket').last().before(_sectionBtn);
+                                if (mw.config.get('skin') === 'minerva') {
+                                    $('.mw-parser-output .mw-editsection').css({ 'display': 'flex', 'align-items': 'center' });
+                                    $(this).append(_sectionBtn);
+                                } else {
+                                    $(this).find('.mw-editsection-bracket').last().before(_sectionBtn);
+                                }
                             } catch (e) {
                                 throwError('fail_to_init_quickedit');
                             }
@@ -1542,6 +1555,9 @@ $(function () {
                 key: 'addFunctionButton',
                 value: function addFunctionButton(text, id, clickEvent) {
                     var button = $('<li></li>').attr('id', id).append($('<a></a>').attr('href', 'javascript:void(0);').text(text));
+                    if (mw.config.get('skin') === 'minerva') {
+                        return;
+                    }
                     if ($('#p-cactions').length > 0) {
                         $('#p-cactions ul').append(button);
                         $('#p-cactions ul').find('li').last().click(clickEvent);
@@ -1732,7 +1748,7 @@ $(function () {
                 _classCallCheck(this, Wikiplus);
 
                 this.version = '2.3.2';
-                this.langVersion = '211';
+                this.langVersion = '212';
                 this.releaseNote = '修正一些问题';
                 this.notice = new MoeNotification();
                 this.inValidNameSpaces = [-1, 8964];
