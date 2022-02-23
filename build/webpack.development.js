0d099d0c1a7c597dc451295a84ab5ab2fa63d520
Eridanus Sora <sora@sound.moe>
2022-02-24 00:27:21 +0800

    fix webpack config

--- build/webpack.development.js
+++ build/webpack.development.js
@@ -1,8 +1,13 @@
+const path = require("path");
 const baseConfig = require("./webpack.base");
 
 module.exports = {
     ...baseConfig,
     mode: "development",
+    output: {
+        path: path.resolve(__dirname, "../dist/"),
+        filename: "Main.development.js",
+    },
     devtool: "source-map",
     optimization: {
         usedExports: true,
